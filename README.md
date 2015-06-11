056765dd32f269c5fa06162900ade6f94d36b550
Kenneth Kan <kenhkan@gmail.com>
2015-06-11 02:45:28 -0400

    Template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Haskell in Vagrant
+
+A simple template to start using GHC/Cabal in Vagrant
---
+++ vagrant.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Haskell Toolbelt: https://www.stackage.org/install
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install -y cabal-install-1.20 ghc-7.8.4
+
+# Link Cabal and GHC
+cat >> ~/.bashrc <<EOF
+export PATH=~/.cabal/bin:/opt/cabal/1.20/bin:/opt/ghc/7.8.4/bin:$PATH
+EOF
+export PATH=~/.cabal/bin:/opt/cabal/1.20/bin:/opt/ghc/7.8.4/bin:$PATH
+
+# Required Cabal binaries
+cabal update
+cabal install alex happy
